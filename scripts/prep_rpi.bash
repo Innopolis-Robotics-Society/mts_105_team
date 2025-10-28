8c32a2ae410c835b05dc219f324ffe5ce1ae33cc
Evgenii Shlomov <e.shlomov@innopolis.university>
2025-10-28 16:11:58 +0300

    feat: add rosbag and docker

--- scripts/prep_rpi.bash
+++ scripts/prep_rpi.bash
@@ -2,18 +2,6 @@
 # Get keys for docker
 sudo apt-get update
 
-sudo apt install -y x11vnc xvfb xauth openbox xterm x11-xserver-utils
-Xvfb :0 -screen 0 1920x1080x60 -ac 
-DISPLAY=:0 openbox-session &
-mkdir -p ~/.vnc
-x11vnc -storepasswd ~/.vnc/passwd
-chmod 600 ~/.vnc/passwd
-export DISPLAY=:0
-xhost +SI:localuser:$(whoami)
-# xterm : To debug
-x11vnc -create -usepw -forever -shared -env X11VNC_CREATE_GEOM=1920x1080x60
-
-
 sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
@@ -32,18 +20,20 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
 # Get repo
 git clone --branch dev https://github.com/Innopolis-Robotics-Society/mts_105_team.git
 
-x11vnc -create -usepw -forever -shared -env X11VNC_CREATE_GEOM=1920x1080x24
-
-#------------------------
-
-export DISPLAY=:1
-xhost +SI:localuser:$(whoami)
-
 # Go to repo
 cd mts_105_team/
 
-git submodule init
-git submodule update
+#git submodule init
+#git submodule update
 
 # Get and start container
 sudo docker compose up --build terminal-rpi
+
+
+
+
+
+
+
+
+
